07ca3a6ab15aea09c98ad2802cddb45e66c498c3
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-16 16:18:40 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./assets/styles/App.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
