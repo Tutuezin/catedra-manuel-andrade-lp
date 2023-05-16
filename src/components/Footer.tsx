58d71c11bf7132635d102d3571af3cc3ab507ad6
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-16 19:56:02 -0300

    feat: create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-end items-center p-10 mt-5">
+      <div className="bg-ufrpe w-[180px] h-[80px] bg-center bg-no-repeat bg-cover "></div>
+    </div>
+  );
+}
