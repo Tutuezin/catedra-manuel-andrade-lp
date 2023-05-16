d38c0a8fed985eed13bfea0106749cb7c850f923
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-16 19:56:12 -0300

    feat: create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <div>Header</div>
+      <div className="bg-manuelcorreia w-[20rem] h-[20rem] bg-center bg-no-repeat bg-cover"></div>
     </>
   );
 }
