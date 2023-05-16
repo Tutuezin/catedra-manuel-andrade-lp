8ac3990d414ca73489ed390d3364e5e67e3da02e
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-16 19:55:30 -0300

    chore: add fonts and colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
 import "./assets/styles/index.css";
+import Favicon from "react-favicon";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Favicon url="https://www.researchgate.net/profile/Flamarion-Dutra-Alves/publication/340655700/figure/fig1/AS:880619714273281@1586967509580/Figura-1-Foto-de-Manuel-Correia-de-Andrade_Q320.jpg" />
     <App />
   </React.StrictMode>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,24 @@ module.exports = {
   mode: "JIT",
   content: ["./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      anton: ["Anton", "sans-serif"],
+      castoro: ["Castoro Titling", "cursive"],
+      righteous: ["Righteous", "cursive"],
+      dmsans: ["DM Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        paper: "#f4f4f4",
+        charcoal: "#373737",
+        silk: "#dcd0c0",
+        palegold: "#c0b283",
+      },
+      backgroundImage: {
+        manuelcorreia: "url(../../assets/imgs/manuelCorreiaPic.png)",
+        ufrpe: "url(../../assets/imgs/ufrpePic.png)",
+      },
+    },
   },
   plugins: [
     plugin(function ({ addComponents }) {
