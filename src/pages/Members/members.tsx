62843ea061d6dcc7a7a2c360591db2d3ca5903f3
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-19 21:52:09 -0300

    chore: create members array

---
+++ src/pages/Members/members.tsx
@@ -0,0 +1,7 @@
+export const members = [
+  "bg-rita w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
+  "bg-rhuan w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
+  "bg-gerlane w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
+  "bg-ingrid w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
+  "bg-jaime w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
+];
