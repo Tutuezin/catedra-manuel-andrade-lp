7baa38b85cbb27db5f7d184d4aedfdc30b18c041
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-23 23:45:58 -0300

    chore: set new tracking on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function Button({ title, id }: Props) {
   return (
-    <button className="mb-4 w-[500px] h-[45px] border-2 border-silk bg-paper text-charcoal text-[1.1rem] font-dmsans font-medium transition duration-300 hover:text-white hover:border-paper hover:bg-silk ">
+    <button className="mb-4 w-[500px] h-[45px] border-2 border-silk bg-paper text-charcoal text-[1.1rem] font-dmsans font-medium tracking-wider transition duration-300 hover:text-white hover:border-paper hover:bg-silk">
       <a href={`${id}`}> {title}</a>
     </button>
   );
