6068402144f08c3f0f468d5a4008c696170241d0
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-05-24 01:43:01 -0300

    feat: mock members

--- src/pages/Members/members.tsx
+++ src/pages/Members/members.tsx
@@ -1,7 +1,50 @@
-export const members = [
-  "bg-rita w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
-  "bg-rhuan w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
-  "bg-gerlane w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
-  "bg-ingrid w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
-  "bg-jaime w-[11rem] h-[15rem] bg-center bg-no-repeat bg-cover rounded-tl-[30px] rounded-tr-[9px] rounded-bl-[30px] rounded-br-[60px]",
+import ritaPhoto from "../../assets/imgs/rita.jpeg";
+import rhuanPhoto from "../../assets/imgs/rhuan.jpeg";
+import gerlanePhoto from "../../assets/imgs/gerlane.png";
+import ingridPhoto from "../../assets/imgs/ingrid.jpeg";
+import jaimePhoto from "../../assets/imgs/jaime.jpeg";
+
+export type MemberType = {
+  name: string;
+  photo: string;
+  bg: string;
+  description: string;
+};
+
+export const members: MemberType[] = [
+  {
+    name: "Maria Rita Ivo de Melo Machado ",
+    photo: ritaPhoto,
+    bg: "bg-rita",
+    description:
+      "Possui graduação (2005) e mestrado (2007) em Geografia pela Universidade Federal de Pernambuco e doutorado em Geografia Humana pela Universidade de São Paulo (2013). Foi professora da rede estadual de educação de Pernambuco (2007 - 2009), atuou como docente substituta da Universidade Federal de Pernambuco (2008 - 2009) e da Universidade Federal Rural de Pernambuco (abril de 2012 e fevereiro de 2013), onde atualmente é professora adjunta no Departamento de História. Coordenadora Editorial da Revista Rural e Urbano da UFPE. Líder do Grupo de Pesquisa Rede de pesquisadores - Itinerários da Educação Geográfica (RePEG) e vice-líder do Grupo de Pesquisa: Produção do espaço, Metropolização e Relação Rural - Urbano (GPRU). Participou do Programa de Pós-Graduação em Extensão Rural e Desenvolvimento Local (2017 - 2019) e atualmente é docente do Programa de Pós-Graduação em Agroecologia e Desenvolvimento Territorial (2023), ambos da UFRPE. Desde maio de 2022 é Coordenadora da Cátedra Manuel Correia de Andrade - UFRPE.",
+  },
+  {
+    name: "Rhuann Rodrigo Oliveira",
+    photo: rhuanPhoto,
+    bg: "bg-rhuan",
+    description:
+      "Licenciado em Geografia da Universidade de Pernambuco, Mestrando no Programa de Pós-graduação em Planejamento Territorial e Desenvolvimento Socioambiental (PPGPLAN) da Universidade do Estado de Santa Catarina. Pesquisador do Grupo de Pesquisa e Desenvolvimento do Território, Espaço e Região (GPDeTER), pesquisador do Rede de Pesquisadores - Itinerários da Educação geográfica (RePEG). Fez parte do programa da Capes da Residência Pedagógica, além de monitor de Geografia Agraria na Universidade de Pernambuco. Atualmente é Estagiário em Tópicos de Geografia Urbana pela UDESC e membro da Cátedra Manuel Correia de Andrade - UFRPE desde maio de 2022.",
+  },
+  {
+    name: "Gerlane Gomes da Rocha",
+    photo: gerlanePhoto,
+    bg: "bg-gerlane",
+    description:
+      "Graduanda em Geografia (Licenciatura) pela Universidade Federal de Pernambuco (UFPE). Participante do Programa de Educação Tutorial- PET Geografia / UFPE (SESu/ MEC) desenvolvendo a tríade universitária: ensino, pesquisa e extensão. É integrante do Grupo de Pesquisa de História do Pensamento Geográfico e Epistemologia da Geografia (GEHPEG), desenvolvendo pesquisas auxiliares na área do Pensamento Geográfico Regional e Pernambucano e do Grupo de pesquisa Rede Pesquisadores - Itinerários da Educação Geográfica (RePEG). Faz parte do corpo discente da Cátedra Manuel Correia de Andrade que está vinculada a Universidade Federal Rural de Pernambuco (UFRPE). Desenvolveu entre 2019 e 2022 projetos de Iniciação Científica (Pibic/UFPE/CNPq) na aréa de Ciências Humanas com destaque para a História do Pensamento Geográfico; Geografia Regional; Espaço agrário e Manuel Correia de Andrade. Foi Professora e monitora voluntária entre 2020 e 2021 dos pré-acadêmicos solidários: Interação, Portal e Cavest, ambos vinculados à Pró-Reitoria de Extensão e Cultura (PROEXC) da UFPE. Atualmente compõe o Programa Institucional de Residência Pedagógica (PIRP UFPE, 2022/2024).",
+  },
+  {
+    name: "Ingrid Klebyane Farias de Luna Barbosa",
+    photo: ingridPhoto,
+    bg: "bg-ingrid",
+    description:
+      "Mestranda em Sociologia pela Universidade Federal de Pernambuco e Graduada no curso de Bacharelado em Ciências Sociais pela Universidade Federal Rural de Pernambuco. Bolsista da Fundação de Amparo a Ciência e Tecnologia de PE (FACEPE). Foi pesquisadora do Programa Institucional de Bolsas de Iniciação Cientifica (PIBIC/CNPq) na UFRPE e do Programa de Iniciação Científica Voluntária (PIC) na FUNDAJ, com pesquisas nas áreas de Sociologia Urbana e Sociologia da Educação. Atualmente é componente do corpo editorial da Revista Rural e Urbano (UFRPE/UFPE) e da Revista de Estudos AntiUtilitarista e Poscolinais (UFPE). Integrante dos grupos de pesquisa: Ecologia Política e Direito à Cidade (UFRPE), Laboratório de Estudos e Ensino sobre o Recife (RecLab- UFRPE), Cátedra Manuel Correia de Andrade (UFRPE) e Docência na Educação (GPDE - FUNDAJ).",
+  },
+  {
+    name: "Jaime de Lima Guimarães ",
+    photo: jaimePhoto,
+    bg: "bg-jaime",
+    description:
+      'Mestrando em História Social da Cultura Regional pela Universidade Federal Rural de Pernambuco (UFRPE) - Bolsista CAPES com a pesquisa: "Parque Nacional do Catimbau: um patrimônio. E a comunidade?", Graduado em Licenciatura em História (UFRPE), Professor Pesquisador, Historiador sob Registro Profissional (ME) - 0000011/PE associado à Associação Nacional de História (ANPUH), à Associação Brasileira de Ensino de História (ABEH), à Sociedade de Arqueologia Brasileira (SAB). Membro do Grupo de Pesquisa no CNPq "Produção do espaço, metropolização e relação rural-urbano" na Linha de Pesquisa: "Dinâmicas espaciais no rural e no urbano", do Grupo de Pesquisa no CNPq "Rede de Pesquisadores - Itinerários da Educação geográfica (RePEG)" na Linha de Pesquisa "Mediação didática desenvolvedora de habilidades e competências: uma realidade escolar?", do Laboratório de Estudos e Ensino sobre o Recife (RecLab) e da Cátedra Manuel Correia de Andrade da UFRPE. Também é consultor do Instituto Ouricuri (Alagoinha - PE) e discente no curso de Bacharelado em Arquitetura e Urbanismo pela Faculdade de Ciências Humanas ESUDA (FCHE) em Recife - PE. Técnico em Contabilidade, 2º Tenente Oficial de Engenharia do Exército Brasileiro (R2), Fotógrafo Amador, Designer e Produtor Gráfico (Comunicação Visual) com experiência em criação e produção de impressos gráficos em geral e de sinalização interna e externa de grande porte. Ex-chefe de gabinete do líder do governo na Câmara de Vereadores do Recife (CVR) e Ex-chefe de gabinete do Vice-prefeito de Olinda na Prefeitura Municipal de Olinda (PMO), com experiência em assessoria de comunicação política estratégica.',
+  },
 ];
